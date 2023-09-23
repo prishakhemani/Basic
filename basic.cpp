842ddcda223478a3bed10d51875ce87b95da8fc2
Prisha Khemani <142494518+prishakhemani@users.noreply.github.com>
2023-09-23 11:09:46 +0530

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -4,12 +4,29 @@ using namespace std;
 int myintvar = 345;
 float myfloatvar = 50.89;
 char mycharvar = 'P';
-string mySentence = "ENTC";
+string mystr = "ENTC";
 int yourint;
-float yourPerc;
+float yourfloat;
 char yourchar;
 string yourstring;
 
 int main() {  
-   
+     cout<<"my integer variable is" << myintvar <<"\n";
+    cout<<"My float variable is " << myfloatvar <<"\n";
+    cout<<"My character variable is " << mycharvar <<"\n";
+    cout<<"Size of that character is " << sizeof(mycharvar) <<"\n";
+    cout<<"The string is = "<<mystr <<"\n";
+    cout<<"Size of that string is " << sizeof(mystr) <<"\n";
+    
+    cout<<"Name an integer value: ";
+    cin>>yourint;
+    cout<<"float value: ";
+    cin>>yourPerc;
+    cout<<"Name a character: ";
+    cin>>yourchar;
+    cout<<"Write a string: ";
+    cin>>yourstring;
+    cout<<"\n the integer value is "<<yourint<<", float value: "<<yourfloat <<", the character was "<<yourchar<<" and that string was "<<yourstring;    
+    return 0;
+}
  
